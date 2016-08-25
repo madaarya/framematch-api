83ee1bad069691f12ec4aaf2d2b28f86c155f913
Mada Aryakusumah <mada@41studio.com>
2016-08-25 15:33:55 +0700

    remove validation

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -3,7 +3,5 @@ class Frame < ApplicationRecord
   dragonfly_accessor :frame
 
   validates :frame, presence: true
-  validates_size_of :frame, maximum: 500.kilobytes, message: "should be no more than 500 KB", if: :frame_changed?
-
   validates_property :format, of: :frame, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: "should be either .jpeg, .jpg, .png, .bmp", if: :frame_changed?
 end
