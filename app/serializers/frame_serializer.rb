c1cb2caaa808c4065f9999c4cc93369a66620d2a
Mada Aryakusumah <mada@41studio.com>
2016-09-06 08:25:32 +0700

    setup thumb url

--- app/serializers/frame_serializer.rb
+++ app/serializers/frame_serializer.rb
@@ -1,8 +1,12 @@
 class FrameSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image_url
+  attributes :id, :title, :image_url, :thumb_url
 
   def image_url
     object.frame.thumb("#{@instance_options[:width_screen]}x#{@instance_options[:height_size]}#").url
+  end
+
+  def thumb_url
+    object.frame.thumb("480x320#").url
   end
 
 end
