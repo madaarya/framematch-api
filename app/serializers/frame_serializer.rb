20779d8170a7757a967777a0b64e350a37326de4
Mada Aryakusumah <mada@41studio.com>
2016-09-13 11:34:06 +0700

    provide API for potrait frame

--- app/serializers/frame_serializer.rb
+++ app/serializers/frame_serializer.rb
@@ -5,6 +5,10 @@ class FrameSerializer < ActiveModel::Serializer
     object.frame.thumb("#{@instance_options[:width_screen]}x#{@instance_options[:height_size]}#").url
   end
 
+  def potrait_image_url
+    object.frame.thumb("#{@instance_options[:height_size]}x#{@instance_options[:width_screen]}#").url
+  end
+
   def thumb_url
     object.frame.thumb("480x320#").url
   end
