372e8025654848bdc5d754face722681f3f8ba10
Mada Aryakusumah <mada@41studio.com>
2016-09-13 16:45:27 +0700

    add potrait to json responds

--- app/serializers/frame_serializer.rb
+++ app/serializers/frame_serializer.rb
@@ -1,5 +1,5 @@
 class FrameSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image_url, :thumb_url
+  attributes :id, :title, :image_url, :thumb_url, :potrait_image_url
 
   def image_url
     object.frame.thumb("#{@instance_options[:width_screen]}x#{@instance_options[:height_size]}#").url
