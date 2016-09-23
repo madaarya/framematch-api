30177b364c1bbf96799a3e04e7d5cd1bcfca178e
Mada Aryakusumah <mada@41studio.com>
2016-09-23 14:41:44 +0700

    add rollbar and fix typo

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -3,5 +3,5 @@ class Prop < ApplicationRecord
   dragonfly_accessor :prop
 
   validates :prop, presence: true
-  validates_property :format, of: :prop, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: "should be either .jpeg, .jpg, .png, .bmp", if: :frame_changed?
+  validates_property :format, of: :prop, in: [:jpeg, :jpg, :png, :bmp], case_sensitive: false, message: "should be either .jpeg, .jpg, .png, .bmp", if: :prop_changed?
 end
